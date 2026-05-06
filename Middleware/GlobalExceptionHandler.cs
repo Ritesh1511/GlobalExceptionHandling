aa2a7123273b59928edaea20797d83ad0e6b0996
Ritesh Kaushik <rkaushik@ivp.in>
2026-05-06 21:23:48 +0530

    first commit

---
+++ Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,36 @@
+﻿namespace GlobalExceptionHandling.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                _logger.LogInformation("Handling request: {Method} {Path}", context.Request.Method, context.Request.Path);
+                await _next(context);
+                _logger.LogInformation("Response Status Code: {StatusCode}",context.Response.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/json";
+
+                var result = new
+                {
+                    Message = "Something went wrong",
+                    Error = ex.Message
+                };
+
+                await context.Response.WriteAsJsonAsync(result);
+            }
+        }
+    }
+}
